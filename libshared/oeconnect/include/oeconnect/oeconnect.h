6288cbfaf50f7f038da2343be8795f81eaf4f501
jyotirmay-neuro <101052218+jyotirmay-neuro@users.noreply.github.com>
2026-06-11 23:19:23 +0530

    feat(libshared): umbrella header

---
+++ libshared/oeconnect/include/oeconnect/oeconnect.h
@@ -0,0 +1,13 @@
+/* libshared/oeconnect/include/oeconnect/oeconnect.h */
+#ifndef OEC_OECONNECT_H
+#define OEC_OECONNECT_H
+
+#include "oeconnect/version.h"
+#include "oeconnect/types.h"
+#include "oeconnect/frame.h"
+#include "oeconnect/ringbuf.h"
+#include "oeconnect/shm.h"
+#include "oeconnect/drift.h"
+#include "oeconnect/sidecar.h"
+
+#endif /* OEC_OECONNECT_H */
