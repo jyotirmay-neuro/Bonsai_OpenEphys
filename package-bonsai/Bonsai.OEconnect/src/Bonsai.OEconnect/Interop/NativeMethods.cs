8c9d958bb19e2ab006f11a88b83a81e228d3bd16
jyotirmay-neuro <101052218+jyotirmay-neuro@users.noreply.github.com>
2026-06-12 21:18:08 +0530

    feat(bonsai): P/Invoke bindings to liboeconnect

---
+++ package-bonsai/Bonsai.OEconnect/src/Bonsai.OEconnect/Interop/NativeMethods.cs
@@ -0,0 +1,96 @@
+using System;
+using System.Runtime.InteropServices;
+
+namespace Bonsai.OEconnect.Interop;
+
+internal static class NativeMethods
+{
+    private const string Lib = "oeconnect";
+
+    /* --- frame --- */
+    [DllImport(Lib, EntryPoint = "oec_frame_init", CallingConvention = CallingConvention.Cdecl)]
+    public static extern void FrameInit(ref OecFrameHeader header,
+        ushort streamId, uint payloadLen, ulong sampleIndex,
+        ulong hostQpcTicks, ushort flags);
+
+    [DllImport(Lib, EntryPoint = "oec_frame_validate", CallingConvention = CallingConvention.Cdecl)]
+    public static extern OecStatus FrameValidate(in OecFrameHeader header);
+
+    [DllImport(Lib, EntryPoint = "oec_crc16", CallingConvention = CallingConvention.Cdecl)]
+    public static extern ushort Crc16(IntPtr data, UIntPtr len);
+
+    /* --- region --- */
+    [DllImport(Lib, EntryPoint = "oec_region_size", CallingConvention = CallingConvention.Cdecl)]
+    public static extern UIntPtr RegionSize(
+        uint slotSize, uint slotCount,
+        uint cmdSlotSize, uint cmdSlotCount,
+        uint ackSlotSize, uint ackSlotCount);
+
+    [DllImport(Lib, EntryPoint = "oec_region_open", CallingConvention = CallingConvention.Cdecl)]
+    public static extern OecStatus RegionOpen(IntPtr mem, UIntPtr memLen, out IntPtr outHeader);
+
+    /* --- ringbuf --- */
+    [DllImport(Lib, EntryPoint = "oec_ringbuf_attach", CallingConvention = CallingConvention.Cdecl)]
+    public static extern OecStatus RingbufAttach(IntPtr regionMem, int kind, out IntPtr outRing);
+
+    [DllImport(Lib, EntryPoint = "oec_ringbuf_detach", CallingConvention = CallingConvention.Cdecl)]
+    public static extern void RingbufDetach(IntPtr rb);
+
+    [DllImport(Lib, EntryPoint = "oec_ringbuf_peek", CallingConvention = CallingConvention.Cdecl)]
+    public static extern IntPtr RingbufPeek(IntPtr rb, out uint outSize);
+
+    [DllImport(Lib, EntryPoint = "oec_ringbuf_consume", CallingConvention = CallingConvention.Cdecl)]
+    public static extern void RingbufConsume(IntPtr rb);
+
+    [DllImport(Lib, EntryPoint = "oec_ringbuf_acquire", CallingConvention = CallingConvention.Cdecl)]
+    public static extern IntPtr RingbufAcquire(IntPtr rb, int dropOldest, out uint outSize);
+
+    [DllImport(Lib, EntryPoint = "oec_ringbuf_publish", CallingConvention = CallingConvention.Cdecl)]
+    public static extern void RingbufPublish(IntPtr rb);
+
+    /* --- shm --- */
+    [DllImport(Lib, EntryPoint = "oec_shm_open", CallingConvention = CallingConvention.Cdecl, CharSet = CharSet.Ansi)]
+    public static extern OecStatus ShmOpen(
+        [MarshalAs(UnmanagedType.LPStr)] string name, UIntPtr expectedSize,
+        out IntPtr outShm, out IntPtr outMapped, out UIntPtr outMappedSize);
+
+    [DllImport(Lib, EntryPoint = "oec_shm_close", CallingConvention = CallingConvention.Cdecl)]
+    public static extern void ShmClose(IntPtr shm);
+
+    /* --- drift --- */
+    [DllImport(Lib, EntryPoint = "oec_drift_create", CallingConvention = CallingConvention.Cdecl)]
+    public static extern IntPtr DriftCreate();
+
+    [DllImport(Lib, EntryPoint = "oec_drift_destroy", CallingConvention = CallingConvention.Cdecl)]
+    public static extern void DriftDestroy(IntPtr fit);
+
+    [DllImport(Lib, EntryPoint = "oec_drift_add", CallingConvention = CallingConvention.Cdecl)]
+    public static extern void DriftAdd(IntPtr fit, ulong sampleIndex, ulong qpc);
+
+    [DllImport(Lib, EntryPoint = "oec_drift_fit", CallingConvention = CallingConvention.Cdecl)]
+    public static extern OecStatus DriftFit(IntPtr fit, out double outA, out double outB);
+
+    [DllImport(Lib, EntryPoint = "oec_drift_predict_qpc", CallingConvention = CallingConvention.Cdecl)]
+    public static extern ulong DriftPredictQpc(IntPtr fit, ulong sampleIndex);
+
+    /* --- sidecar --- */
+    [StructLayout(LayoutKind.Sequential, Pack = 1)]
+    public struct OecSidecar
+    {
+        public int Pid;
+        [MarshalAs(UnmanagedType.ByValTStr, SizeConst = 64)] public string ShmRegion;
+        [MarshalAs(UnmanagedType.ByValTStr, SizeConst = 64)] public string DataEvent;
+        [MarshalAs(UnmanagedType.ByValTStr, SizeConst = 64)] public string CmdEvent;
+        [MarshalAs(UnmanagedType.ByValTStr, SizeConst = 64)] public string ZmqFallbackEndpoint;
+        [MarshalAs(UnmanagedType.ByValTStr, SizeConst = 64)] public string ZmqCmdEndpoint;
+        [MarshalAs(UnmanagedType.ByValTStr, SizeConst = 16)] public string SpecVersion;
+        public ulong StartedUnixNs;
+    }
+
+    [DllImport(Lib, EntryPoint = "oec_sidecar_read", CallingConvention = CallingConvention.Cdecl)]
+    public static extern OecStatus SidecarRead(int pid, out OecSidecar outSidecar);
+
+    [DllImport(Lib, EntryPoint = "oec_sidecar_dir", CallingConvention = CallingConvention.Cdecl, CharSet = CharSet.Ansi)]
+    public static extern OecStatus SidecarDir(
+        [Out] byte[] outBuf, UIntPtr outBufLen);
+}
---
+++ package-bonsai/Bonsai.OEconnect/src/Bonsai.OEconnect/Interop/NativeStructs.cs
@@ -0,0 +1,71 @@
+using System.Runtime.InteropServices;
+
+namespace Bonsai.OEconnect.Interop;
+
+[StructLayout(LayoutKind.Sequential, Pack = 1)]
+public struct OecFrameHeader
+{
+    public uint Magic;
+    public byte VersionMajor;
+    public byte VersionMinor;
+    public ushort StreamId;
+    public uint PayloadLen;
+    public ulong SampleIndex;
+    public ulong HostQpcTicks;
+    public ushort Flags;
+    public ushort Crc16;
+}
+
+[StructLayout(LayoutKind.Sequential, Pack = 1)]
+public struct OecBlockSubheader
+{
+    public ushort NumChannels;
+    public ushort NumSamples;
+    public byte Dtype;
+    public byte SourceId;
+    public ushort Reserved;
+}
+
+public enum OecStatus
+{
+    Ok                  = 0,
+    EInvalidArg         = -1,
+    EBadMagic           = -2,
+    EVersionMismatch    = -3,
+    EFrameTooLarge      = -4,
+    ERingFull           = -5,
+    ERingEmpty          = -6,
+    ESyscall            = -7,
+    ENoSession          = -8,
+    EParse              = -9,
+    ENotImplemented     = -10
+}
+
+public static class OecStreams
+{
+    public const ushort RawBlock      = 0x0001;
+    public const ushort FilteredBlock = 0x0002;
+    public const ushort Spike         = 0x0003;
+    public const ushort TtlEvent      = 0x0004;
+    public const ushort Sync          = 0x0010;
+    public const ushort Cmd           = 0x0020;
+    public const ushort Ack           = 0x0021;
+    public const ushort Error         = 0x0022;
+}
+
+public static class OecCmds
+{
+    public const ushort StartRecord = 0x0001;
+    public const ushort StopRecord  = 0x0002;
+    public const ushort SetTtl      = 0x0003;
+    public const ushort PulseTtl    = 0x0004;
+    public const ushort StartAcq    = 0x0005;
+    public const ushort StopAcq     = 0x0006;
+    public const ushort GetState    = 0x0007;
+}
+
+public enum OecAckStatus : ushort
+{
+    Ok = 0, Pending = 1, Completed = 2, Busy = 3,
+    NotSupported = 4, BadArg = 5, Timeout = 6, Internal = 7
+}
